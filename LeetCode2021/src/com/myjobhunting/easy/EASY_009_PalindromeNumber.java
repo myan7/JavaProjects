b30c38d407986d8dbf473b45bd34420a99ffc8f4
Administrator <myan7@hawk.iit.edu>
2021-10-31 11:20:24 -0500

    Palindrome number completed

--- LeetCode2021/src/com/myjobhunting/easy/EASY_009_PalindromeNumber.java
+++ LeetCode2021/src/com/myjobhunting/easy/EASY_009_PalindromeNumber.java
@@ -24,6 +24,12 @@ import java.rmi.server.ExportException;
 import java.util.Stack;
 
 public class EASY_009_PalindromeNumber {
+
+   /*  The first idea that comes to mind is to convert the number into string,
+   and check if the string is a palindrome,
+   but this would require extra non-constant space for creating the string
+   which is not allowed by the problem description.
+    */
     public boolean isPalindrome(int intVal){
         String str = String.valueOf(intVal);
         int start = 0;
@@ -34,23 +40,14 @@ public class EASY_009_PalindromeNumber {
         return true;
     }
 
+    /*
+    Second idea would be reverting the number itself,
+    and then compare the number with original number,
+    if they are the same, then the number is a palindrome.
+    However, if the reversed number is larger than \text{int.MAX}int.MAX,
+    we will hit integer overflow problem.
+     */
     public boolean isPalindrome2(int intVal){
-        int revertedNumber = 0;
-        if(intVal < 0 || intVal% 10 == 0 && intVal  != 0 )
-        {
-            return false;
-        }
-        else
-        {
-            while(intVal > revertedNumber) {
-                revertedNumber = revertedNumber * 10 + intVal % 10;
-                intVal /= 10;
-            }
-        }
-        return intVal == revertedNumber || intVal == revertedNumber/10;
-    }
-
-    public boolean isPalindrome3(int intVal){
         if(intVal <0 ) return false;
 
         String str = String.valueOf(intVal);
@@ -73,7 +70,57 @@ public class EASY_009_PalindromeNumber {
         }
 
         return reversedVal == intVal;
-    } // isPalindrome3 ends
+    } // isPalindrome2 ends
+
+    /*
+    Following the thoughts based on the second idea,
+    to avoid the overflow issue of the reverted number,
+    what if we only revert half of the \text{int}int number?
+    After all, the reverse of the last half of the palindrome
+    should be the same as the first half of the number, if the number is a palindrome.
+
+    For example,
+    if the input is 1221,
+    if we can revert the last part of the number "1221" from "21" to "12",
+    and compare it with the first half of the number "12",
+    since 12 is the same as 12, we know that the number is a palindrome.
+
+    Time complexity : O(\log_{10}(n))
+    We divided the input by 10 for every iteration, so the time complexity is O(log10(n))
+    Space complexity : O(1).
+    * */
+    public boolean isPalindrome3( int x ){
+        // Special cases:
+        // As discussed above, when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if (x < 0 || ( x != 0 && x % 10 == 0))
+            return false;
+        int rev = 0;
+        while (rev < x )
+        {
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        return x == rev || x == rev/10;
+    }
+
+    public boolean isPalindrome3_v1( int x ){
+        if (x < 0 || ( x != 0 && x % 10 == 0))
+            return false;
+        int rev = 0;
+        while (rev <= x ) // including x == 0
+        {
+            rev = rev*10 + x%10;
+            if( rev == x ) // for length is odd
+                return true;
+            x = x/10;
+            if( rev == x ) // for length is even
+                return true;
+        }
+        return false;
+    }
 
     public boolean isPalindrome4(int x) {
         int n,sum=0,y,i;
@@ -100,5 +147,7 @@ public class EASY_009_PalindromeNumber {
         }
         return true;
     }
+
+
 
 } // class ends

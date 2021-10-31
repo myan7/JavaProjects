edfdbe87c327cfa860e13f4aaa5eeef33b8770ac
Administrator <myan7@hawk.iit.edu>
2021-10-31 10:52:05 -0500

    Palindrome number completed isPanlindrome5 added

--- LeetCode2021/src/com/myjobhunting/easy/EASY_009_PalindromeNumber.java
+++ LeetCode2021/src/com/myjobhunting/easy/EASY_009_PalindromeNumber.java
@@ -64,7 +64,7 @@ public class EASY_009_PalindromeNumber {
         {
             intArr[i++] = stack.pop();
         }
-        try
+        try // for scenario 1234567899, which reverse will exceed integer.
         {
             reversedVal = Integer.parseInt(new String(intArr)) ;
         }catch (Exception e)
@@ -89,7 +89,16 @@ public class EASY_009_PalindromeNumber {
             return x == sum;
         }
         else return x == 0;
+    }
 
+    public boolean isPalindrome5( int x ){
+        String str = String.valueOf(x);
+        for (int i = 0; i < str.length();i++ )
+        {
+            if(str.charAt(i) != str.charAt(str.length()-1-i))
+                return false;
+        }
+        return true;
     }
 
 } // class ends

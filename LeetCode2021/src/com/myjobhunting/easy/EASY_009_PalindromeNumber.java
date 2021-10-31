b44c4949af9732d6cb990c0ccd343018a9c92871
Administrator <myan7@hawk.iit.edu>
2021-10-31 03:29:25 -0500

    Palindrome number completed

---
+++ LeetCode2021/src/com/myjobhunting/easy/EASY_009_PalindromeNumber.java
@@ -0,0 +1,78 @@
+package com.myjobhunting.easy;
+
+/*
+Given an integer x, return true if x is palindrome integer.
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+Example 1:
+Input: x = 121
+Output: true
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+Example 4:
+
+Input: x = -101
+Output: false
+*/
+
+import java.rmi.server.ExportException;
+import java.util.Stack;
+
+public class EASY_009_PalindromeNumber {
+    public boolean isPalindrome(int intVal){
+        String str = String.valueOf(intVal);
+        int start = 0;
+        int end = str.length() - 1;
+        while(start < end){
+            if(str.charAt(start++) != str.charAt(end--)) return false;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome2(int intVal){
+        int revertedNumber = 0;
+        if(intVal < 0 || intVal% 10 == 0 && intVal  != 0 )
+        {
+            return false;
+        }
+        else
+        {
+            while(intVal > revertedNumber) {
+                revertedNumber = revertedNumber * 10 + intVal % 10;
+                intVal /= 10;
+            }
+        }
+        return intVal == revertedNumber || intVal == revertedNumber/10;
+    }
+
+    public boolean isPalindrome3(int intVal){
+        if(intVal <0 ) return false;
+
+        String str = String.valueOf(intVal);
+        char[] intArr = str.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : intArr) stack.push(c);
+
+        int i = 0, reversedVal;
+        while(!stack.isEmpty())
+        {
+            intArr[i++] = stack.pop();
+        }
+        try
+        {
+            reversedVal = Integer.parseInt(new String(intArr)) ;
+        }catch (Exception e)
+        {
+            return false;
+        }
+
+        return reversedVal == intVal;
+    } // isPalindrome3 ends
+
+} // class ends
--- LeetCode2021/src/com/myjobhunting/easy/Main.java
+++ LeetCode2021/src/com/myjobhunting/easy/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        /* 001 two sum */
+        /* 009 Palindrome number */
+        //int val = 121;
+        //int val = -121;
+        //int val = 10;
+        int val = 1234567899;
+        EASY_009_PalindromeNumber solution = new EASY_009_PalindromeNumber();
+        System.out.println(solution.isPalindrome3(val));
+
+
+        /* 001 two sum
         int[] arr = new int[]{3,2,0,4,5};
         int target = 6;
 
@@ -12,5 +21,7 @@ public class Main {
         int[] ans = solution.twoSum(arr,target);
 
         System.out.println("The answer to easy 001 two sum is ["+ ans[0]+", "+ ans[1]+ "]." );
+        */
+
     }
 }
